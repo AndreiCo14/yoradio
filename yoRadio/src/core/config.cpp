2eb517ea842b471f0084ae67ed43d47bcd115912
AndreiCo14 <andrey_co@tut.by>
2025-04-04 21:08:04 +0300

    bug 2nd sch

--- yoRadio/src/core/config.cpp
+++ yoRadio/src/core/config.cpp
@@ -373,11 +373,11 @@ void Config::setDefaults() {
   store.sch_vol = 3;
   store.sch_dow1 = 0b00111110;//0, вс, пн...
   //Расписание 2
-  strlcpy(store.sch_on2,"07:00", 6);//Время включения
-  strlcpy(store.sch_off2,"07:00", 6);//Время выключения
+  strlcpy(store.sch_on2,"11:00", 6);//Время включения
+  strlcpy(store.sch_off2,"11:00", 6);//Время выключения
   store.sch_sta2 = 0;
   store.sch_vol2 = 3;
-  store.sch_dow2 = 0b00111110;//0, вс, пн...
+  store.sch_dow2 = 0b01000001;//0, вс, пн...
   eepromWrite(EEPROM_START, store);
 }
 
